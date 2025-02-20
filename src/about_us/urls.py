096679ca2f4012d8e9171df1c995fe9c5dcf0233
Renat Security Consult LTD <it@renatsecurity.com>
2025-02-20 14:23:40 +0100

    Improve account app UI

--- src/about_us/urls.py
+++ src/about_us/urls.py
@@ -4,9 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.about_us, name='about_us'),
     path('mission-vision/', views.mission_vision, name='about_mission_vision'),
-    path('mission-vision/<slug:slug>/', views.mission_vision, name='about_mission_vision'),
+    path('mission-vision/<slug:slug>/', views.mission_vision_detail, name='about_mission_vision_detail'),
     path('corporate-profile/', views.corporate_profile, name='about_corporate_profile'),
-    path('corporate-profile/<slug:slug>/', views.corporate_profile, name='about_corporate_profile'),
+    path('corporate-profile/<slug:slug>/', views.corporate_profile_detail, name='about_corporate_profile_detail'),
     path('partners/', views.partners_list, name='about_partners'),
     path('partners/<slug:slug>/', views.partner_detail, name='about_partner_detail'),
     path('mentors/', views.mentor_list, name='about_mentors'),
--- src/about_us/views.py
+++ src/about_us/views.py
@@ -12,22 +12,22 @@ def about_us(request):
     return render(request, 'about_us/about.html', {'about': about})
 
 
-# def mission_vision(request):
-#     mission = MissionVision.objects.first()
-#     return render(request, 'about_us/mission_vision.html', {'mission': mission})
+def mission_vision(request):
+    mission = MissionVision.objects.first()
+    return render(request, 'about_us/mission_vision.html', {'mission': mission})
 
 
-def mission_vision(request, slug):
+def mission_vision_detail(request, slug):
     mission = get_object_or_404(MissionVision, slug=slug)
     return render(request, 'about_us/mission_vision.html', {'mission': mission})
 
 
-# def corporate_profile(request):
-#     profile = CorporateProfile.objects.first()
-#     return render(request, 'about_us/corporate_profile.html', {'profile': profile})
+def corporate_profile(request):
+    profile = CorporateProfile.objects.first()
+    return render(request, 'about_us/corporate_profile.html', {'profile': profile})
 
 
-def corporate_profile(request, slug):
+def corporate_profile_detail(request, slug):
     profile = get_object_or_404(CorporateProfile, slug=slug)
     return render(request, 'about_us/corporate_profile.html', {'profile': profile})
 

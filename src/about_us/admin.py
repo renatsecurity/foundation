63f0047b936ee98fab7a3c1f07b99b9b6ef2535d
Renat Security Consult LTD <it@renatsecurity.com>
2025-02-21 11:05:37 +0100

    Modify and improve about page UI

--- src/about_us/admin.py
+++ src/about_us/admin.py
@@ -1,10 +1,5 @@
 from django.contrib import admin
-from .models import (
-    AboutUs,
-    MissionVision, CorporateProfile,
-    AboutPartner, AboutMentor,
-    GetInvolved, UpcomingEvent
-)
+from .models import AboutUs, CorporateProfile
 
 
 @admin.register(AboutUs)
@@ -12,38 +7,7 @@ class AboutUsAdmin(admin.ModelAdmin):
     list_display = ('title', 'last_updated')
 
 
-@admin.register(MissionVision)
-class MissionVisionAdmin(admin.ModelAdmin):
-    list_display = ('title', 'slug', 'last_updated')
-    prepopulated_fields = {"slug": ("title",)}
-
-
 @admin.register(CorporateProfile)
 class CorporateProfileAdmin(admin.ModelAdmin):
     list_display = ('title', 'slug', 'last_updated')
     prepopulated_fields = {"slug": ("title",)}
-
-
-@admin.register(AboutPartner)
-class AboutPartnerAdmin(admin.ModelAdmin):
-    list_display = ('name', 'slug', 'website')
-    prepopulated_fields = {"slug": ("name",)}
-
-
-@admin.register(AboutMentor)
-class AboutMentorAdmin(admin.ModelAdmin):
-    list_display = ('name', 'slug')
-    prepopulated_fields = {"slug": ("name",)}
-
-
-@admin.register(GetInvolved)
-class GetInvolvedAdmin(admin.ModelAdmin):
-    list_display = ('title', 'slug', 'last_updated')
-    prepopulated_fields = {"slug": ("title",)}
-
-
-@admin.register(UpcomingEvent)
-class UpcomingEventAdmin(admin.ModelAdmin):
-    list_display = ('title', 'slug', 'date', 'location')
-    prepopulated_fields = {"slug": ("title",)}
-
--- src/about_us/urls.py
+++ src/about_us/urls.py
@@ -3,16 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.about_us, name='about_us'),
-    path('mission-vision/', views.mission_vision, name='about_mission_vision'),
-    path('mission-vision/<slug:slug>/', views.mission_vision_detail, name='about_mission_vision_detail'),
     path('corporate-profile/', views.corporate_profile, name='about_corporate_profile'),
     path('corporate-profile/<slug:slug>/', views.corporate_profile_detail, name='about_corporate_profile_detail'),
-    path('partners/', views.partners_list, name='about_partners'),
-    path('partners/<slug:slug>/', views.partner_detail, name='about_partner_detail'),
-    path('mentors/', views.mentor_list, name='about_mentors'),
-    path('mentors/<slug:slug>/', views.mentor_detail, name='about_mentor_detail'),
-    path('get-involved/', views.get_involved, name='about_get_involved'),
-    path('get-involved/<slug:slug>/', views.get_involved_detail, name='about_get_involved'),
-    path('upcoming-events/', views.upcoming_events, name='about_upcoming_events'),
-    path('upcoming-events/<slug:slug>/', views.upcoming_event_detail, name='about_upcoming_event_detail'),
 ]

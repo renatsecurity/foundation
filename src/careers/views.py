e122e376c392d6388edf1f6a9ce076b5f8fe4433
Renat Security Consult LTD <it@renatsecurity.com>
2025-02-28 10:25:06 +0100

    Refactor UI

--- src/careers/views.py
+++ src/careers/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
 from .models import Career
 
 
 def career_list(request):
     careers = Career.objects.all().order_by('-posted_date')
-    return render(request, 'careers/career_list.html', {'careers': careers})
+    # Paginate news
+    paginator = Paginator(careers, 10)  # 10 items per page
+
+    page_number = request.GET.get('page')  # Get current page number
+    page_obj = paginator.get_page(page_number)  # Get page object
+
+    return render(request, 'careers/career_list.html', {'page_obj': page_obj})
 
 
 def career_detail(request, slug):
